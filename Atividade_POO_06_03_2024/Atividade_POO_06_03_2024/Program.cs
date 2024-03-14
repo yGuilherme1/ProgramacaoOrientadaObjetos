344fe32f6deffc69a49984f73fe73031a15f0fa0
yGuilherme1 <161392187+yGuilherme1@users.noreply.github.com>
2024-03-13 20:43:35 -0400

    Add files via upload

--- Atividade_POO_06_03_2024/Atividade_POO_06_03_2024/Program.cs
+++ Atividade_POO_06_03_2024/Atividade_POO_06_03_2024/Program.cs
@@ -1,3 +1,19 @@
 ﻿using static System.Console;
 using static System.Convert;
 
+Voo v = new Voo(1, DateTime.Now, 100);
+
+v.Ocupa(10);
+
+WriteLine(v.Verifica(9));
+
+/* TESTES
+v.poltronas[0] = true;
+WriteLine(v.ProximoLivre());
+
+
+for(int i = 0; i < v.qtdVagas; i++)
+{
+    WriteLine(v.poltronas[i]);
+}
+*/
--- Atividade_POO_06_03_2024/Atividade_POO_06_03_2024/Voo.cs
+++ Atividade_POO_06_03_2024/Atividade_POO_06_03_2024/Voo.cs
@@ -1,19 +1,76 @@
 ﻿public class Voo
 {
-    public string numeroVoo;
-    public DateTime data;
-    public int ocupacaoMax;
-    public int ocupacaoAtual;
+    private int numVoo;
+    private DateTime dataHoraVoo;
+    public int qtdVagas;
+    public bool[] poltronas;
 
-    public Voo(string numeroVoo, DateTime data, int ocupacaoMax)
+    public Voo(int numVoo, DateTime dataHoraVoo, int qtdVagas)
     {
-        this.numeroVoo = numeroVoo;
-        this.data = data;
-        this.ocupacaoMax = ocupacaoMax;
+        this.numVoo = numVoo;
+        this.dataHoraVoo = dataHoraVoo;
+        this.qtdVagas = qtdVagas;
+        poltronas = new bool[qtdVagas];
     }
 
-    public int ProximoLivre() 
+    public int ProximoLivre()
     {
+        for (int i = 0; i < poltronas.Length; i++)
+        {
+            if (poltronas[i] == false)
+            {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
 
+    public bool Verifica(int nPoltrona)
+    {
+        if (poltronas[nPoltrona - 1] == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool Ocupa(int nPoltrona)
+    {
+        if (this.Verifica(nPoltrona) == false)
+        {
+            poltronas[nPoltrona - 1] = true;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int Vagas()
+    {
+        int vagasLivres = 0;
+        for(int i = 0; i < poltronas.Length; i++)
+        {
+            if (poltronas[i] == false)
+            {
+                vagasLivres += 1;
+            }
+        }
+
+        return vagasLivres;
+    }
+
+    public int GetVoo()
+    {
+        return numVoo;
+    }
+
+    public DateTime GetData()
+    {
+        return dataHoraVoo;
     }
 }
