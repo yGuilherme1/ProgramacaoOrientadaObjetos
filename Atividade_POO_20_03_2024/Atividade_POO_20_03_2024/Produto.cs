9a8904fb6710817b408e3e971db8bbd51c3837c3
yGuilherme1 <161392187+yGuilherme1@users.noreply.github.com>
2024-03-20 22:24:16 -0400

    Add files via upload

---
+++ Atividade_POO_20_03_2024/Atividade_POO_20_03_2024/Produto.cs
@@ -0,0 +1,58 @@
+﻿using static System.Console;
+
+public class Produto
+{
+    public string id;
+    public string descricao;
+    public string unidade;
+    public int quantidade;
+    public double preco;
+
+    public Produto(string id, string descricao, string unidade, double preco, int quantidade)
+    {
+        this.id = id;
+        this.descricao = descricao;
+        this.unidade = unidade;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public void SetPreco(double preco)
+    {
+        if (preco > 0)
+        {
+            this.preco = preco;
+        }
+        else
+        {
+            WriteLine("Valor Invalido");
+        }
+    }
+
+    public double GetPreco()
+    {
+        return preco;
+    }
+
+    public void SetUnidade(string unidade)
+    {
+        unidade = unidade.ToUpper();
+        if (unidade == "LT" 
+            || unidade == "CX" 
+            || unidade == "KG" 
+            || unidade == "UN"
+            || unidade == "MT") 
+        { 
+            this.unidade = unidade;
+        }
+        else
+        {
+            WriteLine("Unidade não permitida");
+        }
+    }
+
+    public string GetUnidade()
+    {
+        return unidade;
+    }
+}
---
+++ Atividade_POO_20_03_2024/Atividade_POO_20_03_2024/Program.cs
@@ -0,0 +1,76 @@
+﻿using static System.Console;
+using static System.Convert;
+
+List<Produto> listaProdutos = new List<Produto>();
+
+while (true)
+{
+    WriteLine(@" 
+    1. Cadastrar Produto           
+    2. Listar Produtos         
+    3. Remover Produto
+    4. Sair");
+
+    WriteLine("\nEscolha uma opção: ");
+    int op = ToInt32(ReadLine());
+
+    switch (op)
+    {
+        case 1:
+            WriteLine("Digite o id do produto: ");
+            string id = ReadLine();
+            WriteLine("Digite a descrição do produto: ");
+            string descricao = ReadLine();
+            WriteLine("Digite a unidade do produto (LT, CX, KG, UN, MT): ");
+            string unidade = ReadLine().ToUpper();
+            WriteLine("Digite o preço do produto: ");
+            double preco = ToDouble(ReadLine());
+            WriteLine("Digite a quantidade no estoque: ");
+            int quantidade = ToInt32(ReadLine());
+
+            Produto novoProduto = new Produto(id, descricao, unidade, preco, quantidade);
+            listaProdutos.Add(novoProduto);
+            WriteLine("Produto cadastrado com sucesso!");
+
+            break;
+
+        case 2:
+            if (listaProdutos.Count > 0)
+            {
+                WriteLine("Lista de Produtos: ");
+                foreach (Produto produto in listaProdutos)
+                {
+                    WriteLine($"Id: {produto.id}, Descrição: {produto.descricao}, Unidade: {produto.unidade} " +
+                        $"Preço: {produto.preco} Quantidade: {produto.quantidade}.");
+                }
+            }
+            else
+            {
+                WriteLine("Nenhum produto cadastrado.");
+            }
+            break;
+
+        case 3:
+            WriteLine("Digite o id do produto.");
+            id = ReadLine();
+
+            Produto remocao = listaProdutos.Find(produto => produto.id == id);
+            if(remocao != null)
+            {
+                listaProdutos.Remove(remocao);
+                WriteLine("Produto removido com sucesso!");
+            }
+            else
+            {
+                WriteLine("Produto não encontrado.");
+            }
+            break;
+
+        case 4:
+            return;
+
+        default:
+            Write("Opção Invalida.");
+            break;
+    }
+}
